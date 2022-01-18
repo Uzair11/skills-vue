2d12f865d8f9cad793d89b43682f91d6a0e49fc3
Uzair11 <uzairimtiaz44@gmail.com>
2022-01-18 22:44:18 +0500

    added contact page navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,11 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/contactus',
+    name: 'contactUs',
+    component: () => import(/* webpackChunkName: "contactUs" */ '../views/ContactUs.vue'),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/ContactUs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="contactUs">
+    <h1>This is an contact us page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {},
+    };
+  },
+};
+</script>
